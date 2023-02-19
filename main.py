dd8d5cb65e7e8e0d051a66c742add589d83d5097
R-E-T <118467405+R-E-T@users.noreply.github.com>
2023-02-19 16:50:53 +0300

    Update main.py
    добавили движение дороги

--- main.py
+++ main.py
@@ -16,20 +16,40 @@ def load_image(name, colorkey=None):
     image = pygame.image.load(fullname)
     return image
 
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, number, *args):
+        self.image = load_image('fon.png').convert()
+        self.rect = self.image.get_rect()
+        self._layer = -10
+        pygame.sprite.Sprite.__init__(self, *args)
+        self.moved = 0
+        self.number = number
+        self.rect.y = -self.rect.height * self.number
+
+    def update(self):
+        self.rect.move_ip(0, 1)
+        self.moved += -1
+
+        if self.moved <= -self.rect.height:
+            self.rect.y = -self.rect.height * self.number
+            self.moved = 0
+
+
 # создадим группу, содержащую все спрайты
 all_sprites = pygame.sprite.Group()
 
 # создадим спрайт
 sprite = pygame.sprite.Sprite()
-bg_img = pygame.sprite.Sprite()
+#bg_img = pygame.sprite.Sprite()
 # определим его вид
 sprite.image = load_image("car.png")
-bg_img.image = load_image("fon.png")
+#bg_img.image = load_image("fon.png")
 # и размеры
-bg_img.rect = bg_img.image.get_rect()
+#bg_img.rect = bg_img.image.get_rect()
 sprite.rect = sprite.image.get_rect()
 # добавим спрайт в группу
-all_sprites.add(bg_img)
+#all_sprites.add(bg_img)
 all_sprites.add(sprite)
 
 sprite.rect.x = 550
@@ -37,10 +57,17 @@ sprite.rect.y = 580
 
 right = False
 left = False
-
+group = pygame.sprite.LayeredUpdates()
+Background(0, group)
+Background(1, group)
+clock = pygame.time.Clock()
 if __name__ == '__main__':
     running = True
     while running:
+        group.draw(window)
+        window.fill((0, 0, 0))
+        group.update()
+        group.draw(window)
         all_sprites.draw(window)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -68,4 +95,5 @@ if __name__ == '__main__':
         all_sprites.update()
         pygame.display.update()
     pygame.display.flip()
+    clock.tick(60)
     pygame.quit()
