c5d5405d7f11189b5b9c2cb1000e5615b4f4248b
mishiguns700 <122728475+mishiguns700@users.noreply.github.com>
2023-02-26 18:49:39 +0300

    Update main.py
    последние корректировки(или нет?)

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import pygame
 import sys
 import random
 import os
+
 pygame.init()
 
 cars = ["cop1.png", "car3.png", "car4.png", "car5.png", "car6.png"]
 cars_r = ["cop1_r.png", "car.png", "car4_r.png", "car5_r.png", "car6_r.png"]
 
 width, height = 1024, 1024
+
 window = pygame.display.set_mode((width, height))
 
 
-
-def load_image(name, colorkey=None):
+def load_image(name):
     fullname = os.path.join('data', name)
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
@@ -41,17 +42,23 @@ class Background(pygame.sprite.Sprite):
             self.moved = 0
 
 
-# создадим группу, содержащую все спрайты
+# группа с машинкой-игроком
 all_sprites = pygame.sprite.Group()
+# группа с машинками в левой части экрана
 all_sprites2 = pygame.sprite.Group()
+# группа с машинками в правой части экрана
 all_sprites2_r = pygame.sprite.Group()
+# группа с картинкой конца игры
 all_sprites_gameover = pygame.sprite.Group()
-# создадим спрайт
+# спрайт машинки-игрока
 sprite = pygame.sprite.Sprite()
+# спрайт машинок в левой части экрана
 Cars = pygame.sprite.Sprite()
+# спрайт машинок в правой части экрана
 Cars_r = pygame.sprite.Sprite()
+# спрайт с картинкой конца игры
 gameover = pygame.sprite.Sprite()
-# определим его вид
+# картинки
 sprite.image = load_image("car.png")
 a = random.choice(cars)
 Cars.image = load_image(a)
@@ -63,16 +70,18 @@ Cars.rect = Cars.image.get_rect()
 sprite.rect = sprite.image.get_rect()
 Cars_r.rect = Cars_r.image.get_rect()
 gameover.rect = gameover.image.get_rect()
-# добавим спрайт в группу
+# добавление спрайтов в группы
 all_sprites2.add(Cars)
 all_sprites.add(sprite)
 all_sprites2_r.add(Cars_r)
 all_sprites_gameover.add(gameover)
-
+# определение в левой части полосы на которой будет ехать машинка
 r = random.choice([0, 1])
+# определение в правой части полосы на которой будет ехать машинка
 rr = random.choice([0, 1])
 
 Cars.image = load_image(a)
+
 if r == 1:
     Cars.rect.x = 330
     Cars.rect.y = -560
@@ -81,6 +90,7 @@ else:
     Cars.rect.y = -560
 
 Cars_r.image = load_image(a_r)
+
 if rr == 1:
     Cars_r.rect.x = 550
     Cars_r.rect.y = -560
@@ -93,6 +103,7 @@ sprite.rect.y = 720
 
 right = False
 left = False
+
 group = pygame.sprite.LayeredUpdates()
 Background(0, group)
 Background(1, group)
@@ -108,46 +119,61 @@ if __name__ == '__main__':
         window.fill((0, 0, 0))
         group.update()
         group.draw(window)
+
         all_sprites.draw(window)
         all_sprites2.draw(window)
         all_sprites2_r.draw(window)
+
         window.blit(font.render(text, True, (0, 0, 0)), (32, 48))
+
         r = random.choice([0, 1])
         rr = random.choice([0, 1])
         for event in pygame.event.get():
-            if event.type == pygame.USEREVENT and over == False:
+            if event.type == pygame.USEREVENT and over is False:
                 counter += 1
                 text = str(counter).rjust(3)
+
             if event.type == pygame.QUIT:
                 running = False
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     right = True
+
                 if event.key == pygame.K_LEFT:
                     left = True
+
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     right = False
+
                 if event.key == pygame.K_LEFT:
                     left = False
+
         hits_r = pygame.sprite.spritecollide(sprite, all_sprites2_r, False)
+
         if hits_r:
             all_sprites_gameover.draw(window)
             font = pygame.font.SysFont('Consolas', 100)
             window.blit(font.render(text, True, ("red")), (400, 550))
             over = True
+
         hits = pygame.sprite.spritecollide(sprite, all_sprites2, False)
+
         if hits:
             all_sprites_gameover.draw(window)
             font = pygame.font.SysFont('Consolas', 100)
             window.blit(font.render(text, True, ("red")), (400, 550))
             over = True
-        if Cars_r.rect.y != 1024 and over == False:
+
+        if Cars_r.rect.y != 1024 and over is False:
             Cars_r.rect.y += 2
-        if Cars_r.rect.y == 1024 and over == False:
+
+        if Cars_r.rect.y == 1024 and over is False:
             Cars_r.rect = Cars.image.get_rect()
             a_r = random.choice(cars_r)
             Cars_r.image = load_image(a_r)
+
             if rr == 1:
                 Cars_r.rect.x = 550
                 Cars_r.rect.y = -560
@@ -155,31 +181,40 @@ if __name__ == '__main__':
                 Cars_r.rect.x = 800
                 Cars_r.rect.y = -560
 
-        if Cars.rect.y != 1024 and over == False:
+        if Cars.rect.y != 1024 and over is False:
             Cars.rect.y += 3
-        if Cars.rect.y == 1024 and over == False:
+
+        if Cars.rect.y == 1024 and over is False:
             Cars.rect = Cars.image.get_rect()
             a = random.choice(cars)
             Cars.image = load_image(a)
+
             if r == 1:
                 Cars.rect.x = 330
                 Cars.rect.y = -560
             else:
                 Cars.rect.x = 120
                 Cars.rect.y = -560
+
             all_sprites2.draw(window)
-        if sprite.rect.x == 850 and over == False:
+
+        if sprite.rect.x == 850 and over is False:
             right = False
-        if sprite.rect.x == 70 and over == False:
+
+        if sprite.rect.x == 70 and over is False:
             left = False
-        if right is True and over == False:
+
+        if right is True and over is False:
             sprite.rect.x += 3
             all_sprites.update()
-        if left is True and over == False:
+
+        if left is True and over is False:
             sprite.rect.x -= 3
             all_sprites.update()
+
         all_sprites.update()
         pygame.display.update()
+
     pygame.display.flip()
     clock.tick(120)
     pygame.quit()
